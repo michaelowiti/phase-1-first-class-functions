d2ff2f845e126e9509f342735664b6c299f88cd2
mike <mjowiti@gmail.com>
2023-06-14 15:06:41 +0300

    completed lab

---
+++ index.js
@@ -0,0 +1,24 @@
+
+  
+
+function receivesAFunction(callback){
+    callback()
+
+
+} 
+
+
+
+function  returnsANamedFunction(){
+    function ANamedFunction(){ 
+       
+}
+return ANamedFunction
+
+}
+
+
+function  returnsAnAnonymousFunction(){
+    return function(){}
+
+}
